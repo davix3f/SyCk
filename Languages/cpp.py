8d10a0837655344564103883e5b3a935bb00a0f0
Davide <davide_fiorito@libero.it>
2017-09-08 01:41:30 +0000

    cpp language file

---
+++ Languages/cpp.py
@@ -0,0 +1,33 @@
+#lang:CPP
+#v:0.1b
+
+
+""".
+ -1- any(re.match(operator, "++") for operator in operators) -- implement in For.final_condition
+
+"""
+
+
+class For:
+	init_pattern = r"^for\("  #starting for 'for' cycle
+
+	mid_pattern = {"init_condition":r"int [\w]+[\s]?\=[\s]?[\d]+\;", #initial statement
+			"final_condition":r";(int)?[\w]+(\==|\<|\>|!=|\>=|\<=)[\d]+", #final condition to stop for
+			"increment_operator":r"([\w]+)?[\s]?(\++|\--|\+=[\d]+|\-=[\d]+|\/=[\d]+|\*=[\d]+)\)" #incrementing operator [ex x++]
+                        }
+
+	final_pattern = r"\)[\s]?\{.+\}" # something){codecodecode} -- detector
+
+
+class While:
+	init_pattern = r"while[\s]?\("
+
+	mid_pattern = r"(([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w]+)"
+
+	final_pattern = r"\)[\s]?\{.+\}"
+
+
+class DoWhile:
+	init_pattern = r"do \{.+\}"
+
+	final_pattern = r"while\((([\s]*\([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w])+\)"
