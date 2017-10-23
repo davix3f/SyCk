d83973500fc820e0e47713c804779fc882f839a1
Unknown <davide_fiorito@libero.it>
2017-10-23 17:52:46 +0200

    Minor fixes and changes

--- src/syck.py
+++ src/syck.py
@@ -1,4 +1,4 @@
-import re, cpp, syckIO
+import re, cpp, syckIO, brackets
 
 from linecache import *
 
@@ -11,26 +11,27 @@ for item in cpp.lang_classes:
 lines=syckIO.lines
 
 syckIO.reader()
-#print(lines)
 
 class Elements:
-    
+
     functions={}
 
     function_attributes = ["name","return_type","extension","args","nested"]
 
+    last_parent = None
+
 
 def function_detector():
-    
-    
+
+
     for item in lines:
-        
+
         function_match = re.match(cpp.Function.init_pattern, lines[item])
+
         if function_match:
-    
+
             print("Matching: ", lines[item])
 
             Elements.functions[function_match.group("f_name")]=cpp.Function(name=function_match.group("f_name"),
             return_type = function_match.group("return_type"))
-
-
+            Elements.last_parent = function_match.group("f_name")
