2db254dc37fd72e811b714dfaddce134f3aa7ea4
Unknown <davide_fiorito@libero.it>
2017-10-30 01:06:21 +0100

    Quite hard rewrite
    Classes as classes to be instantiated. WIP

--- Languages/cpp.py
+++ Languages/cpp.py
@@ -1,9 +1,14 @@
 #lang:CPP
-#v:0.3
+#v:0.7
 
-class Function:
-    def __init__(self, name, return_type, extension=None, args=None, nested={}):
-        
+class FunctionClass:
+    def __init__(self,
+                        name,
+                        return_type,
+                        extension=None,
+                        args=None,
+                        nested={}):
+
         self.name = name
         self.return_type = return_type
         self.extension = extension
@@ -11,47 +16,63 @@ class Function:
         self.nested = nested
 
 
-    init_pattern = r"(?P<return_type>(int|bool|char|string|void))[\s]+(?P<f_name>\w+)[\s]*\(((int|bool|char|string)(.+)?)?\){"
-
-    final_pattern = r"}$"
-
-class For:
-
-	init_pattern = r"^for\("  #starting for 'for' cycle
-
-	mid_pattern = {"init_condition":r"(int [\w]+[\s]?\=[\s]?[\d]+\;)|(\;[\s]*)", #initial statement
-			"final_condition":r"((int)?[\w]+(\==|\<|\>|!=|\>=|\<=)[\d]+\;)|(\;[\s]*)", #final condition to stop for
-			"increment_operator":r"(([\w]+)?[\s]?(\++|\--|\+=[\d]+|\-=[\d]+|\/=[\d]+|\*=[\d]+)\))?" #incrementing operator [ex x++]
-                        }
-
-	final_pattern = r"\)[\s]?\{.+\}" # something){codecodecode} -- detector
+class ForClass:
+    pass
 
 
-class While:
 
-	init_pattern = r"while[\s]?\("
-
-	mid_pattern = r"(([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w]+)"
-
-	final_pattern = r"\)[\s]?\{.+\}"
+class WhileClass:
+    pass
 
 
-class DoWhile:
 
-	init_pattern = r"do \{.+\}"
+class DoWhileClass:
+    pass
 
-	final_pattern = r"while\((([\s]*\([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w])+\)"
 
-class Switch(Function):
-        
-	init_pattern = r"switch[\s]*\(([\w]+|[\d]+)\)[\s]*\{"
-
-	mid_pattern = r"case ([\w]+|[\d]+)\:[\n]([\t]*(.*)[\n]*)*break;"
-
-	final_pattern = r"break;((\n)*)(\s*)?\}"
+class SwitchClass:
+    pass
 
 
 
 #List of all langclasses. Keep it updated.
 
-lang_classes = [For, While, DoWhile, Switch, Function]
+lang_classes = [ ForClass, WhileClass, DoWhileClass, SwitchClass, FunctionClass ]
+
+
+Function = {"init_pattern": r"(?P<return_type>(int|bool|char|string|void))[\s]+(?P<f_name>\w+)[\s]*\(((int|bool|char|string)(.+)?)?\)[\s]*[\{]*",
+                    "final_pattern": r"}$"}
+
+
+For = {
+    "init_pattern" : r"^for\(",
+    "mid_pattern" : {"init_condition":r"(int [\w]+[\s]?\=[\s]?[\d]+\;)|(\;[\s]*)", #initial statement
+			                       "final_condition":r"((int)?[\w]+(\==|\<|\>|!=|\>=|\<=)[\d]+\;)|(\;[\s]*)", #final condition to stop for
+			                       "increment_operator":r"(([\w]+)?[\s]?(\++|\--|\+=[\d]+|\-=[\d]+|\/=[\d]+|\*=[\d]+)\))?" #incrementing operator [ex x++]
+                                   },
+    	"final_pattern" : r"\)[\s]?\{.+\}" # something){codecodecode} -- detector
+}
+
+
+While = {
+    "init_pattern" : r"while[\s]?\(",
+
+    "mid_pattern" : r"(([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w]+)",
+
+    "final_pattern" : r"\)[\s]?\{.+\}"
+}
+
+
+DoWhile = {
+    "init_pattern" : r"do \{.+\}",
+    "final_pattern" : r"while\((([\s]*\([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w])+\)"
+}
+
+
+Switch = {
+    "init_pattern" : r"switch[\s]*\(([\w]+|[\d]+)\)[\s]*\{",
+    "mid_pattern" : r"case ([\w]+|[\d]+)\:[\n]([\t]*(.*)[\n]*)*break;",
+    "final_pattern" : r"break;((\n)*)(\s*)?\}"
+}
+
+lang_index = [Function, For, While, DoWhile, Switch]
