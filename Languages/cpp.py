38a8c6e3b883808d276ab281f23ba8890c14433d
Unknown <davide_fiorito@libero.it>
2017-12-13 00:48:29 +0100

    Finder function adjustments, names a bit different
    Loop names changed, added log on/off

--- Languages/cpp.py
+++ Languages/cpp.py
@@ -1,5 +1,5 @@
 #lang:CPP
-#v:0.7
+#v:0.10
 import re
 
 class FunctionClass:
@@ -98,8 +98,8 @@ Function = {"name" : "Function",
                     "final_pattern": r"}$"}
 
 
-For = {
-    "name" : "For",
+For_loop = {
+    "name" : "For_loop",
     "init_pattern" : r"for([\s]*)\(",
     "mid_pattern" : {"init_condition":r"(int [\w]+[\s]?\=[\s]?[\d]+\;)|(\;[\s]*)", #initial statement
 			                       "final_condition":r"((int)?[\w]+(\==|\<|\>|!=|\>=|\<=)[\d]+\;)|(\;[\s]*)", #final condition to stop for
@@ -109,8 +109,8 @@ For = {
 }
 
 
-While = {
-    "name" : "While",
+While_loop = {
+    "name" : "While_loop",
     "init_pattern" : r"while[\s]?\(",
 
     "mid_pattern" : r"(([\w]+[\s]?(\==|\<|\>|!=|\>=|\<=)?[\d]*)|[\w]+)",
@@ -127,19 +127,17 @@ DoWhile = {
 
 
 Switch = {
-    "name" : "Switch",
+    "name" : "switch",
     "init_pattern" : r"(switch)([\s]*)\(([\w]+|[\d]+)\)([\s]*)(\{)*",
     "mid_pattern" : r"case ([\w]+|[\d]+)\:[\n]([\t]*(.*)[\n]*)*break;",
     "final_pattern" : r"break;((\n)*)(\s*)?\}"
 }
 
-lang_index = [Function, For, While, DoWhile, Switch]
+lang_index = [Function, For_loop, While_loop, DoWhile, Switch]
 
-def finder(target):
+def finder(target, log=False): #change log to True to have things written
     for item in lang_index:
         if re.search(item["init_pattern"], target)!=None:
-            print("-- Matching %s statement--" %item["name"])
-            return(True)
-
-
-#any(re.search("for",item["init_pattern"])!=None for item in lang_index)
+            if log != False:
+                print("-- Matching %s statement--" %item["name"])
+            return(True, item["name"])
